a9c32e91675d64c793552a092eba0ac840e53d48
lina mayakovskaya <linamayakovskaya@mail.ru>
2023-06-15 18:23:13 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 6730; //стоимость билета
+        int rublesPerMile = 20; //количество рублей для одной бонусной мили
+
+        int bonus = amount / rublesPerMile;
+
+        System.out.println("Итоговый бонус:" + bonus);
     }
+
 }
